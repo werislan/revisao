d3334c0b1f4fc50d90b33962aaf4cb67a1e51f1d
werislan <werislanpereirasantos@gmail.com>
2020-02-14 10:43:36 -0300

    codigo negativo,positivo e nuo

---
+++ revisar.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>informar se o numero e positivo,negativo ou nulo.</title>
+</head>
+<body>
+<form name="numero" id="numero"  type="text" method="POST" >
+<label>digite um numero:</label>
+<input name="num" id="num"  type="text">
+<input type="submit" name="ok" id="ok" value="ok">
+  </form>
+  
+        
+ <?php
+$n= $_POST['num'];
+
+if ($n>0){
+echo"esse numero e positivos";
+}
+else if($n<0){
+    echo "esse numero e negativo";
+
+}
+else{
+    echo "esse numero e nulo";
+}
+
+ ?>       
+</body>
+</html>
